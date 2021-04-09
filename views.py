382b671f8f73f8f03defe7a4300cc0cffb4a58a3
vocart <72550437+vocart@users.noreply.github.com>
2021-04-09 17:02:26 +0200

    Add files via upload

---
+++ views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Todo
+from .forms import TodoForm
+
+def home(request):
+	form = TodoForm()
+
+	todos = Todo.objects.all()
+
+	todo_data = []
+
+	if request.method =="POST":
+		new_todo = Todo(title = request.POST['title'], message = request.POST['message'])
+		new_todo.save()
+		return redirect('home')
+
+	for todo in todos:
+
+		todo_list = {
+			'name': todo.title, 
+			'message': todo.message
+			}
+		todo_data.append(todo_list)
+
+
+
+	return render(request, 'todo/todo.html', {'todos':todos, 'todo_list':todo_list})
+
+
+def delete_todo(request, todo_title):
+    Todo.objects.filter(title=todo_title).delete()
+    return redirect('home')
